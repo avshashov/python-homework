716266583f7fd1b720aa959a4c30bad189b176f7
Artem Shashov <tauquantum@gmail.com>
2022-09-14 23:29:47 +1000

    completed

--- homework-oop.py
+++ homework-oop.py
@@ -22,7 +22,7 @@ class Student:
         for grade in self.grades.values():
             sum_grade += sum(grade)
             counter_grades += len(grade)
-        return sum_grade / counter_grades
+        return sum_grade / counter_grades if counter_grades > 0 else 0.0
 
     def __str__(self):
         courses_in_progress = ', '.join(self.courses_in_progress)
@@ -72,4 +72,51 @@ class Reviewer(Mentor):
         return f'Имя: {self.name} \nФамилия: {self.surname}'
 
 
+def avg_hm(students, subject):
+    avg_grade = 0
+    for student in students:
+        avg_grade += sum(student.grades[subject])
+    avg_grade /= len(students)
+    return avg_grade
 
+
+def avg_lecturers_grade(lecturers, subject):
+    avg_grade = 0
+    for lecturer in lecturers:
+        avg_grade += sum(lecturer.lecturer_grades[subject])
+    avg_grade /= len(lecturers)
+    return avg_grade
+
+
+student1 = Student('Harry', 'Potter', 'Male')
+student2 = Student('Hermione', 'Granger', 'Female')
+student1.add_finished_course('Git')
+student1.courses_in_progress += ['SQL', 'Python']
+student2.courses_in_progress += ['Git', 'Python']
+
+mentor1 = Mentor('Rubeus', 'Hagrid')
+mentor2 = Mentor('Remus', 'Lupin')
+
+lecturer1 = Lecturer('Severus', 'Snape')
+lecturer2 = Lecturer('Alastor', 'Moody')
+lecturer1.courses_attached += ['SQL', 'Python', 'Git']
+lecturer2.courses_attached += ['SQL', 'Python', 'Git']
+
+reviewer1 = Reviewer('Albus', 'Dumbledore')
+reviewer2 = Reviewer('Minerva', 'McGonagall')
+reviewer1.courses_attached += ['SQL', 'Python', 'Git']
+reviewer2.courses_attached += ['SQL', 'Python', 'Git']
+reviewer1.rate_hw(student1, 'Python', 9)
+reviewer1.rate_hw(student1, 'SQL', 8)
+reviewer2.rate_hw(student2, 'Git', 10)
+reviewer2.rate_hw(student2, 'Python', 10)
+
+student1.rate_lecturer(lecturer1, 'SQL', 7)
+student1.rate_lecturer(lecturer1, 'Python', 6)
+student2.rate_lecturer(lecturer2, 'Git', 3)
+student2.rate_lecturer(lecturer2, 'Python', 8)
+
+print(student1, student2, lecturer1, lecturer2, reviewer1, reviewer2, sep='\n\n', end='\n\n')
+print(student1 > student2, lecturer1 > lecturer2)
+print(avg_hm([student1, student2], 'Python'))
+print(avg_lecturers_grade([lecturer1, lecturer2], 'Python'))
